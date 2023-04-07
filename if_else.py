1de3065ec6caf1b8f20f3205629db37db09fb01b
Mayank Jha <mj5030@srmist.edu.in>
2023-04-07 20:05:06 +0530

    add loop code

---
+++ if_else.py
@@ -0,0 +1,7 @@
+num = 1
+while num <= 10:
+    if num <= 10:
+        print(num)
+    else:
+        break
+    num += 1
---
+++ joinstrings.py
@@ -0,0 +1,4 @@
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string: ")
+joined_string = string1 + " " + string2
+print("Joined string: ", joined_string)
