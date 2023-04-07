f386017071eda4b560d68429f7815cd1b5fff7fb
Mayank Jha <mj5030@srmist.edu.in>
2023-04-07 15:28:08 +0530

    new folders added

---
+++ D1/operations/multiplication.py
@@ -0,0 +1,4 @@
+num1 = float(input("Enter num_1: "))
+num2 = float(input("Enter num_2: "))
+sum = num1 * num2
+print(sum)
---
+++ inputstringnumber.py
@@ -0,0 +1,9 @@
+
+string_input = input("Enter a string: ")
+
+
+number_input = int(input("Enter a number: "))
+
+
+print("String: ", string_input)
+print("Number: ", number_input)
